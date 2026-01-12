1bde03b8dd57db58c18d1f5d39654ac63eab9e0e
Alexmag75 <aleksandrmargrarit@gmail.com>
2026-01-12 18:53:33 +0200

    start

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 
 import './App.css'
 
-function App() {
+const coursesTitleArray: string[] = [
+    'JavaScript Complex',
+    'Java Complex',
+    'Python Complex',
+    'QA Complex',
+    'Fullstack',
+    'Frontend'
+];
 
+function App() {
 
   return (
     <>
-старт
+       <ul>
+           {coursesTitleArray.map((value,index) => <li key={index}>{value}</li>)}
+       </ul>
     </>
   )
 }
