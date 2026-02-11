c701f634c0ad20ad1c747bf21916764746201d9f
Alexmag75 <aleksandrmargrarit@gmail.com>
2026-02-11 20:18:17 +0200

    hw/hw3/task3

--- src/components/users/UserDummyComponent.tsx
+++ src/components/users/UserDummyComponent.tsx
@@ -1,3 +1,4 @@
+import './UserComponent.css'
 import {type FC} from "react";
 import type {UserModelDummy} from "../../models/dummy/UserModelDummy.ts";
 type PropType = {
@@ -6,8 +7,15 @@ type PropType = {
 export const UserDummyComponent:FC<PropType> = ({user}) => {
     return (
         <>
-            {user.username}
-            {user.email}
+            <div className='profile-card'>
+                <img src={user.image}></img>
+                <p> {user.firstName} {user.lastName} {user.maidenName} {user.age} лет {user.birthDate}</p>
+                <p> Логин: {user.username} тел {user.phone}  пароль: {user.password} </p>
+                <p> Адрес: {user.address.state} тел {user.address.city} {user.address.address} {user.address.postalCode}</p>
+                <p> Банк: {user.bank.currency} {user.bank.cardType} {user.bank.cardNumber} {user.bank.cardExpire} {user.bank.iban}</p>
+                <p> Компания: {user.company.department} тел {user.company.name}  пароль: {user.company.title} </p>
+                <p> Должность: {user.role}</p>
+            </div>
         </>
     );
 };
--- src/components/users/UserJsonComponent.tsx
+++ src/components/users/UserJsonComponent.tsx
@@ -1,5 +1,6 @@
 import {type FC} from "react";
 import type {UserModel} from "../../models/json/UserModel.ts";
+import './UserComponent.css'
 
 type PropType = {
     user:UserModel
@@ -7,10 +8,14 @@ type PropType = {
 export const UserJsonComponent:FC<PropType> = ({user}) => {
     return (
         <>
-            {user.id}
-            {user.name}
-            {user.username}
-            {user.website}
+            <div className='profile-card'>
+                     <p> {user.id} {user.name} Логин: {user.username}</p>
+                     <p> Email: {user.email}</p>
+                     <p> Телефон: {user.phone}</p>
+                      <p> WebSaite: {user.website}</p>
+                     <p> Адрес: {user.address.city} {user.address.suite} {user.address.street} {user.address.zipcode}</p>
+                     <p> Компания: {user.company.name} {user.company.bs} {user.company.catchPhrase}</p>
+            </div>
         </>
     );
 };
