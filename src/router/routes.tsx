e145eecaa1809b53c405bde884c9607b17990357
Alexmag75 <aleksandrmargrarit@gmail.com>
2026-02-12 18:24:14 +0200

    hw/hw3/task3

---
+++ src/router/routes.tsx
@@ -0,0 +1,18 @@
+import {createBrowserRouter} from "react-router";
+import {MainLayout} from "../layutos/MainLayout.tsx";
+import {UsersPage} from "../layutos/UsersPage.tsx";
+import {PostsPage} from "../layutos/PostsPage.tsx";
+
+
+
+export const routes=createBrowserRouter([
+    {
+        path: '/', element: <MainLayout/>, children:[
+            {path:'users', element:<UsersPage/>},
+            {path:'posts', element:<PostsPage/>}
+        ]
+
+
+    }
+])
+
