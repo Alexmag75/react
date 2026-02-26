e04f48322e31582ce705c92bab6d6127ff389ec2
Alexmag75 <aleksandrmargrarit@gmail.com>
2026-02-26 08:34:51 +0200

    hw/hw6/task1

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
-createRoot(document.getElementById('root')!).render(<App />)
+import {RouterProvider} from "react-router";
+createRoot(document.getElementById('root')!)
+    .render(<RouterProvider router={routes}/>);
